f6848f9585eea67073f1673743a0d26dace6825e
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-06 14:23:57 +0700

    Update wifisettingsdialog.cpp

--- recovery/wifisettingsdialog.cpp
+++ recovery/wifisettingsdialog.cpp
@@ -1,8 +1,8 @@
 /*
  * Wifi network selection dialog
  *
- * Initial author: Floris Bos
- * Maintained by Raspberry Pi
+ * Author: Jean Wallet
+ * Maintained by EFSCOIN Efs
  *
  * See LICENSE.txt for license details
  */
