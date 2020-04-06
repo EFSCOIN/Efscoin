8dbd4e89be85aa4f287abeafbe0e70586bc9c5b9
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-04-06 12:41:09 +0700

    Update confeditdialog.cpp

--- recovery/confeditdialog.cpp
+++ recovery/confeditdialog.cpp
@@ -1,7 +1,7 @@
 /* Configuration edit dialog
  *
- * Initial author: Floris Bos
- * Maintained by Raspberry Pi
+ * Author: Jean Wallet
+ * Maintained by EFSCOIN Efs
  *
  * See LICENSE.txt for license details
  *
